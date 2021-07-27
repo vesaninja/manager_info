fd49d9841a1cb6bcbe7dbb13bb7c1916e57b3c3d
Vesa Järvimäki <vesa.jarvimaki@gmail.com>
2021-07-27 22:03:45 +0300

    telepot -> telegram-bot-api

--- telegram_bot/kiltabotti.py
+++ telegram_bot/kiltabotti.py
@@ -1,91 +1,55 @@
-import telepot
+import logging
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters,ConversationHandler, CallbackContext
 import sys
 import time
 from pprint import pprint
-from telepot.loop import MessageLoop
 import json
 import os
 
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
-######################################################################################################################
-#  Start of helper functions
-######################################################################################################################
-def run_command(command_list, chat_id, msg):
-    """
-    If received text message contains / sign checks if the command is implemented in the script.
-    If corresponding command is found it is called.
-    :param command_list: list of string containing the commands
-    :param chat_id: The id of the chat where the command originated.
-    :param msg: telpot message object containing information about the message.
-    :return: None
-    """
-    # List of command: key is the function to be executed and the list contains all of the keywords that
-    # can be used to call it.
-    commands = {print_help: ["help", "apua"]}
-
-    for function in commands.keys():
-        if command_list[0][1:] in commands[function]:
-            function(str(chat_id), msg, command_list[1:])
-
-
-def check_user(chat_id, name):
-    """
-    Checks if a user with given nickname exists in the json database.
-    :param chat_id: int, Chat in which the user should be.
-    :param name: str, (nick)name of the user.
-    :return: str, id of the user
-    """
-    with open("database.json", 'r') as json_file:
-        database = json.load(json_file)
-
-    if chat_id in database:
-        for user in database[chat_id]:
-            if name in database[chat_id][user]["names"]:
-                return user
 
 ######################################################################################################################
 #  Start of bot commands.
 ######################################################################################################################
 
 
-def print_help(chat_id, msg, command_list):
-    """
-    Gives information about the implemented bot commands.
-    """
-    if len(command_list) == 0:
-        bot.sendMessage(chat_id, "Lista komennoista: \n"
-                                 "Lisää infoa: /help <komennon nimi>")
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text('Ei se mua haittaa. ')
 
 
 ######################################################################################################################
 #  End of bot commands.
 ######################################################################################################################
 
-
-def handle(msg):
+def define_commands(dispatcher):
     """
-    Reads chat messages and determines wheter or not it is a command meant for the bot.
-    :param msg: telpot message object containing information about the messages.
-    :return:
+    Define bot commands!
     """
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    command_list = ['']
-    if content_type == 'text':
-        command_list = msg['text'].split()
-        print("Got command: %s" % command_list[0])
-
-    if command_list[0][0] == "/":
-        run_command(command_list, chat_id, msg)
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
 
 if __name__ == '__main__':
     # Read the api token from locally saved txt file
     with open("token.txt") as token_file:
         token = token_file.read()
-        global bot
-        bot = telepot.Bot(token)
-    # Runs the handle function in the background that listens the chat for possible messages.
-    MessageLoop(bot, handle).run_as_thread()
-    print('I am listening...')
-    while 1:
-        time.sleep(10)
+    updater = Updater(token)
+    dispatcher = updater.dispatcher
+    define_commands(dispatcher)
+
+    # Start the Bot
+    updater.start_polling()
+    # Run bot in background
+    updater.idle()
